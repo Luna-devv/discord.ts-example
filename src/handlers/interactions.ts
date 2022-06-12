8f1b3ab50880c7284ea67c76d87323ff182278b1
Lunish <luni.siaa@gmail.com>
2022-06-12 15:46:00 +0200

    Update interactions.ts

--- src/handlers/interactions.ts
+++ src/handlers/interactions.ts
@@ -5,7 +5,7 @@ export default function () {
     const commands = readdirSync('./dist/interactions/commands/').filter(file => file.endsWith('.js'));
 
     for (let file of commands) {
-        let pull = require(`../interactions/commands/${file}`);
+        let pull = require(`../interactions/commands/${file}`).default;
         if (pull.name) Config.data.interactions.set(pull.name, pull);
     };
 };
