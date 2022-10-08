49f0c45091957f0dac5a86b7c855f352ffe0bc36
Lunish <luni.siaa@gmail.com>
2022-10-09 00:14:25 +0200

    Update ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,12 @@
 import { Client } from 'discord.js';
+import { Interactions } from '../config';
 
 export default {
     name: 'ready',
     once: true,
-    run: (client: Client) => {
-        console.log(client.user.username)
+    run: async (client: Client) => {
+        await client.application?.commands.set(Interactions)
+
+        console.log(client.user?.username)
     }
 }
